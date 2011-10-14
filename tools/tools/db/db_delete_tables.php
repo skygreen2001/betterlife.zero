3b6f956818cba2f63e79c1dc75afbf6f51672fde
skygreen <skygreen2001@gmail.com>
2011-10-14 13:47:00 +0800

    add a tool:delete all datas from tables

--- tools/tools/db/db_delete_tables.php
+++ tools/tools/db/db_delete_tables.php
@@ -3,8 +3,7 @@ require_once ("../../../init.php");
 
 $tableList=Manager_Db::newInstance()->dbinfo()->tableList();
 $symbol_connect="_";
-foreach ($tableList as $tablename){
-    $new_table_name=str_replace($old_prefix.$symbol_connect, $new_prefix.$symbol_connect, $tablename);
+foreach ($tableList as $tablename){                                                                   
     echo "DROP  TABLE $tablename;<br/>";
 }        
 ?>
