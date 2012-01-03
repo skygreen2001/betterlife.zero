960442b54c7ff2c87af7bf949c68c05d46d86b69
skygreen <skygreen2001@gmail.com>
2012-01-03 09:28:30 +0800

    完成Ext显示模板的开发工作

--- tools/tools/db.php
+++ tools/tools/db.php
@@ -5,8 +5,9 @@
   <meta name="description" content="用于DB的工具" />
 </head>
 <body>
-    <a href="db/db_domain.php">数据库生成实体类</a>
-    <a href="db/db_service.php">数据库生成服务类</a><br/>
+    <a href="autoCode/db_domain.php">数据库生成实体类</a>
+    <a href="autoCode/db_service.php">数据库生成服务类</a><br/>
+    <a href="autoCode/view/db_view_ext.php">数据库生成表示层类</a><br/>
     
     <a href="db/rename_db_prefix.php">修改数据库表前缀</a><br/>   
     <a href="db/db_delete_tables.php">删除所有的表</a>
