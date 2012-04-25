4d00b16f566bb63cf128ec9d23adfa920cd3f08c
skygreen <skygreen2001@gmail.com>
2012-04-25 16:25:56 +0800

    完善在linux上的路径问题

--- core/util/view/UtilCss.php
+++ core/util/view/UtilCss.php
@@ -9,278 +9,281 @@
  */
 class UtilCss extends Util 
 {
-    /**
-     * 对CSS进行Gzip操作的路径
-     * @var string 
-     */
-    private static $CSS_GZIP="common/js/gzip.php?css=";
-    /**
-     * JS框架名称键名称
-     * @var string 
-     */
-    private static $CSS_FLAG_GROUP="g";
-    /**
-     * JS框架版本键名称
-     * @var type 
-     */
-    private static $CSS_FLAG_VERSION="v"; 
-    /**
-     * 加载过的css文件。
-     * $value css文件名
-     * @var array 
-     */
-    private static $CssLoaded=array();   
-    
-    const CSS_REPORT_TABLE="report";
-    /**
-     * 动态加载Ext Required CSS文件
-     * @param ViewObject $viewobject 表示层显示对象
-     * @param string $version javascript框架的版本号
-     */
-    public static function loadExt($viewObject=null,$version="3.3.0") 
-    {
-       UtilAjax::init();
-       $g_flag_ext=EnumJsFramework::JS_FW_EXTJS;
-       $ext_resource_root="resources/css/";
-       if ($viewObject){
-           self::loadCssReady($viewObject,$ext_resource_root."ext-all.css",true,$g_flag_ext,$version);
-           self::loadCssReady($viewObject,$ext_resource_root."xtheme-gray.css",true,$g_flag_ext,$version);
-           self::loadCssReady($viewObject,$ext_resource_root."ext-patch.css",true,$g_flag_ext,$version);
-           self::loadCssReady($viewObject,"shared/tabscroller/TabScrollerMenu.css",true,$g_flag_ext,$version);
-       }else{
-           self::loadCss($ext_resource_root."ext-all.css",true,$g_flag_ext,$version);
-           self::loadCss($ext_resource_root."xtheme-gray.css",true,$g_flag_ext,$version);
-           self::loadCss($ext_resource_root."ext-patch.css",true,$g_flag_ext,$version);
-           self::loadCss("shared/tabscroller/TabScrollerMenu.css",true,$g_flag_ext,$version);    
-       }
-    }
-    
-    /**
-     * 动态加载CSS文件
-     * @param string $cssFile Css文件名
-     * @param string $cssFlag 分组标识，如都是Ext的Css文件
-     * @param string $version javascript框架的版本号
-     * @param string $charset 字符集
-     */
-    public static function loadCss($cssFile,$isGzip=false,$cssFlag=null,$version="",$charset="utf-8") 
-    {  
-        echo self::loadCssSentence($cssFile,$isGzip,$cssFlag,$version,$charset);
-    }
-    
-    /**
-     * 预加载[不直接输出]:动态加载CSS文件
-     * @param ViewObject $viewobject 表示层显示对象
-     * @param string $cssFile Css文件名
-     * @param string $cssFlag 分组标识，如都是Ext的Css文件
-     * @param string $version javascript框架的版本号
-     * @param string $charset 字符集
-     */
-    public static function loadCssReady($viewobject,$cssFile,$isGzip=false,$cssFlag=null,$version="",$charset="utf-8") 
-    {
-        if ($viewobject instanceof ViewObject){
-            if (!isset($viewobject->css_ready)||empty($viewobject->css_ready)){
-                $viewobject->css_ready="";
-            }
-            $viewobject->css_ready.=self::loadCssSentence($cssFile,$isGzip,$cssFlag,$version,$charset);
-        }
-    }
-    
-    /**
-     * 动态加载CSS文件的语句
-     * @param string $cssFile Css文件名
-     * @param string $cssFlag 分组标识，如都是Ext的Css文件
-     * @param string $version javascript框架的版本号
-     * @param string $charset 字符集
-     */
-    public static function loadCssSentence($cssFile,$isGzip=false,$cssFlag=null,$version="",$charset="utf-8") 
-    {      
-        $result="";
-        if (isset($cssFile)){
-            $url_base=UtilNet::urlbase();
-            if ($isGzip){
-                if (isset($cssFlag)){
-                    $cssFile.="&".self::$CSS_FLAG_GROUP."=".$cssFlag;
-                }
-                if (!empty($version)){
-                    $cssFile.="&".self::$CSS_FLAG_VERSION."=".$version;
-                }        
-                if (in_array($cssFile, self::$CssLoaded)){
-                    return ;
-                }   
-                $css_gzip=self::$CSS_GZIP;
-                if (contain($cssFile,Gc::$url_base)){
-                    $isLocalCssFile=str_replace("/","\\",str_replace(Gc::$url_base,Gc::$nav_root_path,$cssFile));
-                    $isLocalGzip=dirname(dirname($isLocalCssFile)).DIRECTORY_SEPARATOR."gzip.php";
-                    if (file_exists($isLocalGzip)){
-                        $css_gzip=str_replace(Gc::$nav_root_path,"",$isLocalGzip)."?css=";
-                        $css_gzip=str_replace("\\","/",$css_gzip);
-                    }
-                }
-                $result= "    <link rel=\"stylesheet\" type=\"text/css\" href=\"".$url_base.$css_gzip.$cssFile."\" />\r\n";
-            }else{
-                if (in_array($cssFile, self::$CssLoaded)){
-                    return ;
-                }                    
-                if (startWith($cssFile, "http")){
-                    $result= "    <link rel=\"stylesheet\" type=\"text/css\" href=\"".$cssFile."\" />\r\n";  
-                }else{
-                    $result= "    <link rel=\"stylesheet\" type=\"text/css\" href=\"".$url_base.$cssFile."\" />\r\n";  
-                }
-            }
-            self::$CssLoaded[]=$cssFile;
-        }        
-        return $result;
-    }
-    
-    
-    /**
-     * 动态加载应用指定的Css内容的语句。
-     * @param string $cssContent：Css内容的语句
-     */   
-    public static function loadCssContent($cssContent)
-    {
-        echo self::loadCssContentSentence($cssContent);
-    }
-    
-    /**
-     * 预加载[不直接输出]:动态加载应用指定的Css内容的语句。
-     * @param ViewObject $viewobject 表示层显示对象,只在Web框架中使用,一般结合loadJsReady使用
-     * @param string $cssContent：Js内容的语句
-     */   
-    public static function loadCssContentReady($viewobject,$cssContent)
-    {        
-        if ($viewobject instanceof ViewObject){
-            if (!isset($viewobject->css_ready)||empty($viewobject->css_ready)){
-                $viewobject->css_ready="";
-            }
-            $viewobject->css_ready.=self::loadCssContentSentence($cssContent);
-        }
-    }  
-    
-    /**
-     * 动态加载应用指定的Js内容的语句。
-     * @param string $cssContent：Css内容的语句
-     */   
-    public static function loadCssContentSentence($cssContent)
-    {
-        $result="    <style type=\"text/css\">\r\n";
-        $result.="      ".$cssContent."\r\n";
-        $result.="    </style>\r\n";
-        return $result;
-    }    
+	/**
+	 * 对CSS进行Gzip操作的路径
+	 * @var string 
+	 */
+	private static $CSS_GZIP="common/js/gzip.php?css=";
+	/**
+	 * JS框架名称键名称
+	 * @var string 
+	 */
+	private static $CSS_FLAG_GROUP="g";
+	/**
+	 * JS框架版本键名称
+	 * @var type 
+	 */
+	private static $CSS_FLAG_VERSION="v"; 
+	/**
+	 * 加载过的css文件。
+	 * $value css文件名
+	 * @var array 
+	 */
+	private static $CssLoaded=array();   
+	
+	const CSS_REPORT_TABLE="report";
+	/**
+	 * 动态加载Ext Required CSS文件
+	 * @param ViewObject $viewobject 表示层显示对象
+	 * @param string $version javascript框架的版本号
+	 */
+	public static function loadExt($viewObject=null,$version="3.3.0") 
+	{
+	   UtilAjax::init();
+	   $g_flag_ext=EnumJsFramework::JS_FW_EXTJS;
+	   $ext_resource_root="resources/css/";
+	   if ($viewObject){
+		   self::loadCssReady($viewObject,$ext_resource_root."ext-all.css",true,$g_flag_ext,$version);
+		   self::loadCssReady($viewObject,$ext_resource_root."xtheme-gray.css",true,$g_flag_ext,$version);
+		   self::loadCssReady($viewObject,$ext_resource_root."ext-patch.css",true,$g_flag_ext,$version);
+		   self::loadCssReady($viewObject,"shared/tabscroller/TabScrollerMenu.css",true,$g_flag_ext,$version);
+	   }else{
+		   self::loadCss($ext_resource_root."ext-all.css",true,$g_flag_ext,$version);
+		   self::loadCss($ext_resource_root."xtheme-gray.css",true,$g_flag_ext,$version);
+		   self::loadCss($ext_resource_root."ext-patch.css",true,$g_flag_ext,$version);
+		   self::loadCss("shared/tabscroller/TabScrollerMenu.css",true,$g_flag_ext,$version);    
+	   }
+	}
+	
+	/**
+	 * 动态加载CSS文件
+	 * @param string $cssFile Css文件名
+	 * @param string $cssFlag 分组标识，如都是Ext的Css文件
+	 * @param string $version javascript框架的版本号
+	 * @param string $charset 字符集
+	 */
+	public static function loadCss($cssFile,$isGzip=false,$cssFlag=null,$version="",$charset="utf-8") 
+	{  
+		echo self::loadCssSentence($cssFile,$isGzip,$cssFlag,$version,$charset);
+	}
+	
+	/**
+	 * 预加载[不直接输出]:动态加载CSS文件
+	 * @param ViewObject $viewobject 表示层显示对象
+	 * @param string $cssFile Css文件名
+	 * @param string $cssFlag 分组标识，如都是Ext的Css文件
+	 * @param string $version javascript框架的版本号
+	 * @param string $charset 字符集
+	 */
+	public static function loadCssReady($viewobject,$cssFile,$isGzip=false,$cssFlag=null,$version="",$charset="utf-8") 
+	{
+		if ($viewobject instanceof ViewObject){
+			if (!isset($viewobject->css_ready)||empty($viewobject->css_ready)){
+				$viewobject->css_ready="";
+			}
+			$viewobject->css_ready.=self::loadCssSentence($cssFile,$isGzip,$cssFlag,$version,$charset);
+		}
+	}
+	
+	/**
+	 * 动态加载CSS文件的语句
+	 * @param string $cssFile Css文件名
+	 * @param string $cssFlag 分组标识，如都是Ext的Css文件
+	 * @param string $version javascript框架的版本号
+	 * @param string $charset 字符集
+	 */
+	public static function loadCssSentence($cssFile,$isGzip=false,$cssFlag=null,$version="",$charset="utf-8") 
+	{      
+		$result="";
+		if (isset($cssFile)){
+			$url_base=UtilNet::urlbase();
+			if ($isGzip){
+				if (isset($cssFlag)){
+					$cssFile.="&".self::$CSS_FLAG_GROUP."=".$cssFlag;
+				}
+				if (!empty($version)){
+					$cssFile.="&".self::$CSS_FLAG_VERSION."=".$version;
+				}        
+				if (in_array($cssFile, self::$CssLoaded)){
+					return ;
+				}   
+				$css_gzip=self::$CSS_GZIP;
+				if (contain($cssFile,Gc::$url_base)){                        
+					$isLocalCssFile=str_replace(Gc::$url_base,Gc::$nav_root_path,$cssFile);
+					if (is_server_windows()){
+						$isLocalCssFile=str_replace("/","\\",$isLocalCssFile);
+					}
+					$isLocalGzip=dirname(dirname($isLocalCssFile)).DIRECTORY_SEPARATOR."gzip.php";
+					if (file_exists($isLocalGzip)){
+						$css_gzip=str_replace(Gc::$nav_root_path,"",$isLocalGzip)."?css=";
+						$css_gzip=str_replace("\\","/",$css_gzip);
+					}
+				}
+				$result= "    <link rel=\"stylesheet\" type=\"text/css\" href=\"".$url_base.$css_gzip.$cssFile."\" />\r\n";
+			}else{
+				if (in_array($cssFile, self::$CssLoaded)){
+					return ;
+				}                    
+				if (startWith($cssFile, "http")){
+					$result= "    <link rel=\"stylesheet\" type=\"text/css\" href=\"".$cssFile."\" />\r\n";  
+				}else{
+					$result= "    <link rel=\"stylesheet\" type=\"text/css\" href=\"".$url_base.$cssFile."\" />\r\n";  
+				}
+			}
+			self::$CssLoaded[]=$cssFile;
+		}        
+		return $result;
+	}
+	
+	
+	/**
+	 * 动态加载应用指定的Css内容的语句。
+	 * @param string $cssContent：Css内容的语句
+	 */   
+	public static function loadCssContent($cssContent)
+	{
+		echo self::loadCssContentSentence($cssContent);
+	}
+	
+	/**
+	 * 预加载[不直接输出]:动态加载应用指定的Css内容的语句。
+	 * @param ViewObject $viewobject 表示层显示对象,只在Web框架中使用,一般结合loadJsReady使用
+	 * @param string $cssContent：Js内容的语句
+	 */   
+	public static function loadCssContentReady($viewobject,$cssContent)
+	{        
+		if ($viewobject instanceof ViewObject){
+			if (!isset($viewobject->css_ready)||empty($viewobject->css_ready)){
+				$viewobject->css_ready="";
+			}
+			$viewobject->css_ready.=self::loadCssContentSentence($cssContent);
+		}
+	}  
+	
+	/**
+	 * 动态加载应用指定的Js内容的语句。
+	 * @param string $cssContent：Css内容的语句
+	 */   
+	public static function loadCssContentSentence($cssContent)
+	{
+		$result="    <style type=\"text/css\">\r\n";
+		$result.="      ".$cssContent."\r\n";
+		$result.="    </style>\r\n";
+		return $result;
+	}    
 
-    /**
-     * 列表信息样式报表
-     * @see http://www.somacon.com/p141.php
-     * {self::CSS_REPORT_TABLE}
-     */
-    public static function report_info() 
-    {
-        echo '<style type="text/css">
-            table.'.self::CSS_REPORT_TABLE.' {
-                    border-width: 0px;
-                    border-spacing: ;
-                    border-style: groove;
-                    border-color: ;
-                    border-collapse: separate;
-                    background-color: white;
-            }
-            table.'.self::CSS_REPORT_TABLE.' th {
-                    border-width: 1px;
-                    padding: 1px;
-                    border-style: solid;
-                    border-color: green;
-                    background-color: #fff5ee;
-                    -moz-border-radius: ;
-            }
-            table.'.self::CSS_REPORT_TABLE.' td {
-                    border-width: 1px;
-                    padding: 1px;
-                    border-style: solid;
-                    border-color: green;
-                    background-color: #fff5ee;
-                    -moz-border-radius: ;
-            }
-            </style>';
-    }
-    
-    /**
-     * 定义form的样式
-     */
-    public static function form_css()
-    {
-        echo '<style type="text/css">    
-                html,body {
-                    font:normal 15px SimSun,sans-serif;
-                    border:0 none;
-                    overflow:auto;
-                    line-height:1.5em;
-                    margin:5px 0 0;
-                    padding:0;
-                }
-                div {
-                    margin:15px;
-                }
-                h1 {
-                    font:bold 150% STXingkai;
-                }
-                p#indexPage {
-                    line-height:2em;
-                    width:500px;
-                    padding-left:30px;
-                    text-align:left;
-                }                
-                label {
-                    vertical-align:middle;
-                    width:150px;
-                    height:35px;
-                    text-align:right;
-                    margin:2px 4px 6px;
-                }                
-                input[type=text],input[type=password]{
-                    border:1px solid #FFF;
-                    text-align:center;
-                    width:200px;
-                    height:28px;
-                    line-height:28px;
-                    background:gray;
-                }
-                select{
-                    width:350px;
-                    height:28px;
-                    line-height:28px;
-                    vertical-align:bottom;
-                    text-align:center;                
-                }
-                input[type=button]{
-                    border:1px solid;
-                    text-align:center;
-                    width:80px;
-                    height:28px;
-                }
-                input:hover {
-                    border:1px solid green;
-                    color:#000;
-                    background:#FFF;
-                }
+	/**
+	 * 列表信息样式报表
+	 * @see http://www.somacon.com/p141.php
+	 * {self::CSS_REPORT_TABLE}
+	 */
+	public static function report_info() 
+	{
+		echo '<style type="text/css">
+			table.'.self::CSS_REPORT_TABLE.' {
+					border-width: 0px;
+					border-spacing: ;
+					border-style: groove;
+					border-color: ;
+					border-collapse: separate;
+					background-color: white;
+			}
+			table.'.self::CSS_REPORT_TABLE.' th {
+					border-width: 1px;
+					padding: 1px;
+					border-style: solid;
+					border-color: green;
+					background-color: #fff5ee;
+					-moz-border-radius: ;
+			}
+			table.'.self::CSS_REPORT_TABLE.' td {
+					border-width: 1px;
+					padding: 1px;
+					border-style: solid;
+					border-color: green;
+					background-color: #fff5ee;
+					-moz-border-radius: ;
+			}
+			</style>';
+	}
+	
+	/**
+	 * 定义form的样式
+	 */
+	public static function form_css()
+	{
+		echo '<style type="text/css">    
+				html,body {
+					font:normal 15px SimSun,sans-serif;
+					border:0 none;
+					overflow:auto;
+					line-height:1.5em;
+					margin:5px 0 0;
+					padding:0;
+				}
+				div {
+					margin:15px;
+				}
+				h1 {
+					font:bold 150% STXingkai;
+				}
+				p#indexPage {
+					line-height:2em;
+					width:500px;
+					padding-left:30px;
+					text-align:left;
+				}                
+				label {
+					vertical-align:middle;
+					width:150px;
+					height:35px;
+					text-align:right;
+					margin:2px 4px 6px;
+				}                
+				input[type=text],input[type=password]{
+					border:1px solid #FFF;
+					text-align:center;
+					width:200px;
+					height:28px;
+					line-height:28px;
+					background:gray;
+				}
+				select{
+					width:350px;
+					height:28px;
+					line-height:28px;
+					vertical-align:bottom;
+					text-align:center;                
+				}
+				input[type=button]{
+					border:1px solid;
+					text-align:center;
+					width:80px;
+					height:28px;
+				}
+				input:hover {
+					border:1px solid green;
+					color:#000;
+					background:#FFF;
+				}
 
-                input[type=submit] {
-                    width:100px;
-                    height:32px;
-                    color:#FFF;
-                    cursor:pointer;
-                    font:bold 84% SimSun,helvetica,sans-serif;
-                    background-color:#000;
-                    border:1px solid;
-                    border-color:#696 #363 #363 #696;
-                }
+				input[type=submit] {
+					width:100px;
+					height:32px;
+					color:#FFF;
+					cursor:pointer;
+					font:bold 84% SimSun,helvetica,sans-serif;
+					background-color:#000;
+					border:1px solid;
+					border-color:#696 #363 #363 #696;
+				}
 
-                input[type=submit]:hover{
-                    color:#000;
-                    background-color:#FFF;
-                }
-            </style>';
-    }
+				input[type=submit]:hover{
+					color:#000;
+					background-color:#FFF;
+				}
+			</style>';
+	}
 
 }
 
--- include/system.php
+++ include/system.php
@@ -146,4 +146,220 @@ function alphatonumber($alphabet)
 		return 0;
 	} 
 }
+
+/**
+ * 判断服务器是否Linux 
+ */
+function is_server_windows()
+{
+	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {  
+		return true;  
+	} else {  
+		return false;  
+	}  
+}
+
+/**
+ * 取得阅读器名称和版本
+ * @access public
+ * @return string
+ */
+function getbrowser()
+{
+	global $_SERVER;
+
+	$agent= $_SERVER['HTTP_USER_AGENT'];
+	$browser= '';
+	$browser_ver= '';
+
+	if (preg_match('/OmniWeb\/(v*)([^\s|;]+)/i', $agent, $regs))
+	{
+		$browser='OmniWeb';
+		$browser_ver= $regs[2];
+	}
+
+	if (preg_match('/Netscape([\d]*)\/([^\s]+)/i', $agent, $regs))
+	{
+		$browser='Netscape';
+		$browser_ver= $regs[2];
+	}
+
+	if (preg_match('/safari\/([^\s]+)/i', $agent, $regs))
+	{
+		$browser='Safari';
+		$browser_ver=$regs[1];
+	}
+
+	if (preg_match('/MSIE\s([^\s|;]+)/i', $agent, $regs))
+	{
+		$browser='Internet Explorer';
+		$browser_ver= $regs[1];
+	}
+
+	if (preg_match('/Opera[\s|\/]([^\s]+)/i', $agent, $regs))
+	{
+		$browser='Opera';
+		$browser_ver=$regs[1];
+	}
+
+	if (preg_match('/NetCaptor\s([^\s|;]+)/i', $agent, $regs))
+	{
+		$browser='(Internet Explorer ' .$browser_ver. ') NetCaptor';
+		$browser_ver= $regs[1];
+	}
+
+	if (preg_match('/Maxthon/i', $agent, $regs))
+	{
+		$browser='(Internet Explorer ' .$browser_ver. ') Maxthon';
+		$browser_ver='';
+	}
+
+	if (preg_match('/FireFox\/([^\s]+)/i', $agent, $regs))
+	{
+		$browser='FireFox';
+		$browser_ver=$regs[1];
+	}
+
+	if (preg_match('/Lynx\/([^\s]+)/i', $agent, $regs))
+	{
+		$browser='Lynx';
+		$browser_ver=$regs[1];
+	}
+	if (preg_match('/Chrome\/([^\s]+)/i', $agent, $regs))
+	{
+		$browser='Chrome';
+		$browser_ver=$regs[1];
+	}
+	//echo $agent."<br/>";
+	if ($browser != '')
+	{
+		return $browser.' '.$browser_ver;
+	}
+	else
+	{
+		return 'Unknow browser';
+	}
+}
+
+/**
+ * 取得客户端使用的操作系统体系
+ *
+ * @access private
+ * @return void
+ */
+function client_os()
+{
+	$agent = $_SERVER['HTTP_USER_AGENT'];
+	$os = false;
+
+	if (eregi('win', $agent) && strpos($agent, '95'))
+	{
+		$os = 'Windows 95';
+	}
+	else if (eregi('win 9x', $agent) && strpos($agent, '4.90'))
+	{
+		$os = 'Windows ME';
+	}
+	else if (eregi('win', $agent) && ereg('98', $agent))
+	{
+		$os = 'Windows 98';
+	}
+	else if (eregi('win', $agent) && eregi('nt 6.0', $agent))
+	{
+		$os = 'Windows Vista';
+	}
+	else if (eregi('win', $agent) && eregi('nt 6.1', $agent))
+	{
+		$os = 'Windows 7';
+	}
+	else if (eregi('win', $agent) && eregi('nt 5.1', $agent))
+	{
+		$os = 'Windows XP';
+	}
+	else if (eregi('win', $agent) && eregi('nt 5', $agent))
+	{
+		$os = 'Windows 2000';
+	}
+	else if (eregi('win', $agent) && eregi('nt', $agent))
+	{
+		$os = 'Windows NT';
+	}
+	else if (eregi('win', $agent) && ereg('32', $agent))
+	{
+		$os = 'Windows 32';
+	}
+	else if (eregi('linux', $agent))
+	{
+		$os = 'Linux';
+	}
+	else if (eregi('unix', $agent))
+	{
+		$os = 'Unix';
+	}
+	else if (eregi('sun', $agent) && eregi('os', $agent))
+	{
+		$os = 'SunOS';
+	}
+	else if (eregi('ibm', $agent) && eregi('os', $agent))
+	{
+		$os = 'IBM OS/2';
+	}
+	else if (eregi('Mac', $agent) && eregi('PC', $agent))
+	{
+		$os = 'Macintosh';
+	}
+	else if (eregi('PowerPC', $agent))
+	{
+		$os = 'PowerPC';
+	}
+	else if (eregi('AIX', $agent))
+	{
+		$os = 'AIX';
+	}
+	else if (eregi('HPUX', $agent))
+	{
+		$os = 'HPUX';
+	}
+	else if (eregi('NetBSD', $agent))
+	{
+		$os = 'NetBSD';
+	}
+	else if (eregi('BSD', $agent))
+	{
+		$os = 'BSD';
+	}
+	else if (ereg('OSF1', $agent))
+	{
+		$os = 'OSF1';
+	}
+	else if (ereg('IRIX', $agent))
+	{
+		$os = 'IRIX';
+	}
+	else if (eregi('FreeBSD', $agent))
+	{
+		$os = 'FreeBSD';
+	}
+	else if (eregi('teleport', $agent))
+	{
+		$os = 'teleport';
+	}
+	else if (eregi('flashget', $agent))
+	{
+		$os = 'flashget';
+	}
+	else if (eregi('webzip', $agent))
+	{
+		$os = 'webzip';
+	}
+	else if (eregi('offline', $agent))
+	{
+		$os = 'offline';
+	}
+	else
+	{
+		$os = 'Unknown';
+	}
+	return $os;
+}
 ?>
