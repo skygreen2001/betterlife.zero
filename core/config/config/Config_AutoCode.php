4030d0880cdcd25229e709397e694f9f15874333
skygreen <skygreen2001@gmail.com>
2012-10-21 11:37:45 +0800

    完善后台生成代码

--- core/config/config/Config_AutoCode.php
+++ core/config/config/Config_AutoCode.php
@@ -1,7 +1,7 @@
 <?php
 /**
  +---------------------------------------<br/>
- * 批量生成代码<br/>
+ * 批量生成代码的配置<br/>
  +---------------------------------------
  * @category ele
  * @package core.config.common
@@ -9,13 +9,13 @@
  */
 class Config_AutoCode extends ConfigBB
 {
-    /**
-     * 是否生成的JS文件都在一个文件夹里 
-     */
-    const JSFILE_DIRECT_CORE=false;
-    /**
-     * 一对多关系显示是否完整,默认只生成Grid，完整模式包括增删改分页 
-     */
-    const RELATION_VIEW_FULL=false;
+	/**
+	 * 是否生成的JS文件都在一个文件夹里 
+	 */
+	const JSFILE_DIRECT_CORE=false;
+	/**
+	 * 一对多关系显示是否完整,默认只生成Grid，完整模式包括增删改分页 
+	 */
+	const RELATION_VIEW_FULL=false;
 }
 ?>
