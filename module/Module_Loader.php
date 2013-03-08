956477eacc94b45605a009205a55729dfc44968c
skygreen <root@skygreens-mac.local>
2013-03-08 09:09:12 +0800

    统一webservice调用模块：nusoap

--- module/Module_Loader.php
+++ module/Module_Loader.php
@@ -32,7 +32,7 @@ class Module_Loader {
 	/**
 	 * 加载Module模块的规格Xml文件名。
 	 */
-	const FILE_SPEC_LOAD_MODULE="load.module.xml";    
+	const FILE_SPEC_LOAD_MODULE="load.module.xml";
 	/**
 	 * 加载Module遵循以下规则：
 	 * 1.加载的库文件应该都放在module目录下以加载Module的名称为子目录的名称内
@@ -42,7 +42,8 @@ class Module_Loader {
 	 * 5.open:是否加载Module。yes:加载，no:不加载，如果不定义则代表该Module由逻辑自定义开关规则。
 	 * 6.group:若干Module属于同一个解决方案。
 	 */
-	public static function load_run() {
+	public static function load_run()
+	{
 		$spec_module=UtilXmlSimple::fileXmlToArray(dirname(__FILE__).DIRECTORY_SEPARATOR.self::FILE_SPEC_LOAD_MODULE);
 		foreach ($spec_module["block"] as $block){
 			$blockAttr=$block[Util::XML_ELEMENT_ATTRIBUTES];
@@ -55,29 +56,38 @@ class Module_Loader {
 			}
 		}
 	}
-		
-	/**
-	 * 加载通信接口，可增强和第三方通信的功能 [skygreen原创]
-	 */
-	public static function load_communication(){
-		//加载数据处理方案的所有目录进IncludePath
-		$communication_root_dir=Gc::$nav_root_path.Config_F::ROOT_MODULE.DIRECTORY_SEPARATOR."communication".DIRECTORY_SEPARATOR;
-		//加载模块里所有的文件
-		load_module(Config_F::ROOT_MODULE,$communication_root_dir);
-	}	
-	
+
 	/**
 	 * 加载条形码功能: 引用第三方开源功能
 	 * @link http://www.barcodephp.com
 	 */
-	public static function load_barcode(){
-		
+	public static function load_barcode()
+	{
 		//加载数据处理方案的所有目录进IncludePath
 		$barcode_root_dir=Gc::$nav_root_path.Config_F::ROOT_MODULE.DIRECTORY_SEPARATOR."barcode".DIRECTORY_SEPARATOR;
-		require_once(Gc::$nav_root_path.Config_F::ROOT_MODULE.DIRECTORY_SEPARATOR."barcode".DIRECTORY_SEPARATOR."UtilBarCode.php");        
+		require_once(Gc::$nav_root_path.Config_F::ROOT_MODULE.DIRECTORY_SEPARATOR."barcode".DIRECTORY_SEPARATOR."UtilBarCode.php");
 		$barcode_root_dir.="class".DIRECTORY_SEPARATOR;
 		//加载模块里所有的文件
 		load_module(Config_F::ROOT_MODULE,$barcode_root_dir);
+	}
+
+	/**加载通信模块*/
+	public static function load_communication()
+	{
+		//加载数据处理方案的所有目录进IncludePath
+		$communication_root_dir=Gc::$nav_root_path.Config_F::ROOT_MODULE.DIRECTORY_SEPARATOR."communication".DIRECTORY_SEPARATOR;
+		//加载模块里所有的文件
+		load_module(Config_F::ROOT_MODULE,$communication_root_dir,"webservice");
+		load_module(Config_F::ROOT_MODULE,$communication_root_dir."webservice".DIRECTORY_SEPARATOR,"nusoap");
+	}
+
+	/**加载nusoap模块*/
+	public static function load_nusoap()
+	{
+		$communication_root_dir=Gc::$nav_root_path.Config_F::ROOT_MODULE.DIRECTORY_SEPARATOR."communication".DIRECTORY_SEPARATOR;
+		$nusoap_dir=$communication_root_dir."webservice/nusoap/lib/";
+		require_once($nusoap_dir."nusoap.php");
+		require_once($nusoap_dir."class.wsdlcache.php");
 	}
 }
 
