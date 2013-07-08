5304fa66ed1fd6ffefbb802999bc37bfa77b4331
skygreen2001 <skygreen2001@gmail.com>
2013-07-08 11:28:25 +0800

    完善工具类：替换前缀运行不带前缀

--- tools/tools/db/rename_db_prefix.php
+++ tools/tools/db/rename_db_prefix.php
@@ -3,9 +3,11 @@ require_once ("../../../init.php");
 
 if (isset($_REQUEST["old_prefix"])&&!empty($_REQUEST["old_prefix"])){
 	$old_prefix=$_REQUEST["old_prefix"];
+	if (contain($old_prefix,"_"))$old_prefix=str_replace("_", "", $old_prefix);
 }
 if (isset($_REQUEST["new_prefix"])&&!empty($_REQUEST["new_prefix"])){
 	$new_prefix=$_REQUEST["new_prefix"];
+	if (contain($new_prefix,"_"))$new_prefix=str_replace("_", "", $new_prefix);
 }
 if (!isset($old_prefix)||!isset($old_prefix)){
 	echo  '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -29,8 +31,9 @@ if (!isset($old_prefix)||!isset($old_prefix)){
 }else{
 	$tableList=Manager_Db::newInstance()->dbinfo()->tableList();
 	$symbol_connect="_";
+	if (empty($new_prefix)) $symbol_connect_new=""; else $symbol_connect_new="_";
 	foreach ($tableList as $tablename){
-		$new_table_name=str_replace($old_prefix.$symbol_connect, $new_prefix.$symbol_connect, $tablename);
+		$new_table_name=str_replace($old_prefix.$symbol_connect, $new_prefix.$symbol_connect_new, $tablename);
 		echo "ALTER  TABLE $tablename RENAME TO $new_table_name;<br/>";
 	}
 }
