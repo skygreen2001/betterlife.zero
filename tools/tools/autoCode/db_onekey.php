db42f4b668daaf0509ed8042f65dcc5841642241
skygreen2001 <skygreen2001@gmail.com>
2014-02-02 19:25:25 +0800

    同上

--- tools/tools/autoCode/db_onekey.php
+++ tools/tools/autoCode/db_onekey.php
@@ -1,24 +1,25 @@
 <?php
-require_once ("../../../init.php");                   
+require_once ("../../../init.php");
 if (isset($_REQUEST["save_dir"])&&!empty($_REQUEST["save_dir"]))
-{      
+{
 	$save_dir=$_REQUEST["save_dir"];
-	AutoCodeOneKey::$save_dir =$save_dir; 
-	   
+	AutoCodeOneKey::$save_dir =$save_dir;
+
 	//读取配置文件里查询条件和关系列显示的配置
-	if (file_exists(dirname(__FILE__).DIRECTORY_SEPARATOR."autocode.config.xml")){
-		$classes=UtilXmlSimple::fileXmlToObject(dirname(__FILE__).DIRECTORY_SEPARATOR."autocode.config.xml");  
-		$dataobjects = $classes->xpath("//class");  
+	if (file_exists(dirname(__FILE__).DIRECTORY_SEPARATOR."autocode.config.xml"))
+	{
+		$classes=UtilXmlSimple::fileXmlToObject(dirname(__FILE__).DIRECTORY_SEPARATOR."autocode.config.xml");
+		$dataobjects = $classes->xpath("//class");
 		foreach ($dataobjects as $dataobject) {
 			$attributes=$dataobject->attributes();
-			$classname=$attributes->name."";          
-			$conditions_obj=$dataobject->conditions->condition; 
+			$classname=$attributes->name."";
+			$conditions_obj=$dataobject->conditions->condition;
 			$conditions=array("relation_show"=>array());
 			foreach ($conditions_obj as $condition) {
 				$attributes_condition=$condition->attributes();
 				$condition_name=$condition."";
 				if ($attributes_condition){
-					$con_relation_class=$attributes_condition->relation_class."";  
+					$con_relation_class=$attributes_condition->relation_class."";
 					$show_name=$attributes_condition->show_name."";
 					if (!array_key_exists($condition_name,$conditions["relation_show"])){
 						$conditions["relation_show"][$condition_name]["relation_class"]=$con_relation_class;
@@ -28,33 +29,33 @@ if (isset($_REQUEST["save_dir"])&&!empty($_REQUEST["save_dir"]))
 				if (!in_array($condition_name,$conditions)){
 					$conditions[]=$condition_name;
 				}
-			}                                                              
+			}
 			AutoCodeViewExt::$filter_fieldnames[$classname]=$conditions;
-			
+
 			$relation_viewfield_obj=$dataobject->relationShows->show;
 			if (!empty($relation_viewfield_obj)){
 				$relation_viewfields=array();
 				foreach ($relation_viewfield_obj as $relation_viewfield) {
-					$attributes=$relation_viewfield->attributes(); 
+					$attributes=$relation_viewfield->attributes();
 					$local_key=$attributes->local_key."";
 					if (!array_key_exists($local_key,$relation_viewfields)){
-						$relation_class=$attributes->relation_class."";  
-						$show=array($relation_class=>$relation_viewfield.""); 
+						$relation_class=$attributes->relation_class."";
+						$show=array($relation_class=>$relation_viewfield."");
 						$relation_viewfields[$local_key]=$show;
 					}
-				}                                                                                                            
+				}
 				AutoCodeViewExt::$relation_viewfield[$classname]=$relation_viewfields;
 			}
-			
+
 			$redundancy_table_field_obj=$dataobject->redundancy->table;
 			if (!empty($redundancy_table_field_obj)){
 				$redundancy_table_fields=array();
 				foreach ($redundancy_table_field_obj as $redundancy_table) {
-					$attributes=$redundancy_table->attributes(); 
+					$attributes=$redundancy_table->attributes();
 					$table_name=$attributes->name."";
 					$redundancy_field_obj=$redundancy_table->field;
 					foreach ($redundancy_field_obj as $redundancy_field) {
-						$attributes=$redundancy_field->attributes(); 
+						$attributes=$redundancy_field->attributes();
 						$field_name=$attributes->name."";
 						$field_come=$attributes->come."";
 						if (empty($field_come)) $field_come=$field_name;
@@ -63,16 +64,16 @@ if (isset($_REQUEST["save_dir"])&&!empty($_REQUEST["save_dir"]))
 				}
 				AutoCodeViewExt::$redundancy_table_fields[$classname]=$redundancy_table_fields;
 			}
-			
-			//**********************start:导出数据对象之间关系规范定义*************************       
+
+			//**********************start:导出数据对象之间关系规范定义*************************
 			relation_specification_create($classname,$dataobject);
 			//**********************end  :导出数据对象之间关系规范定义*************************
-		} 
-	}  
-	AutoCodeOneKey::AutoCode();                                                     
+		}
+	}
+	AutoCodeOneKey::AutoCode();
 }  else {
 	AutoCodeOneKey::UserInput();
-}     
+}
 
 /**
  * 导出数据对象之间关系规范定义
@@ -80,46 +81,46 @@ if (isset($_REQUEST["save_dir"])&&!empty($_REQUEST["save_dir"]))
  * @param array dataobject 来自xml配置文件单个类的所有说明
  */
 function relation_specification_create($classname,$dataobject)
-{           
+{
 	//导出一对一关系规范定义(如果存在)
 	$has_one_spec=$dataobject->has_one->relationclass;
-	if (!empty($has_one_spec)){       
+	if (!empty($has_one_spec)){
 		foreach ($has_one_spec as $has_one) {
-			$attributes=$has_one->attributes();  
-			AutoCodeDomain::$relation_all[$classname]['has_one'][$attributes->name.""]=$has_one."";  
-		}                     
-	}                  
+			$attributes=$has_one->attributes();
+			AutoCodeDomain::$relation_all[$classname]['has_one'][$attributes->name.""]=$has_one."";
+		}
+	}
 	//导出从属一对一关系规范定义(如果存在)
 	$belong_has_one_spec=$dataobject->belong_has_one->relationclass;
-	if (!empty($belong_has_one_spec)){        
+	if (!empty($belong_has_one_spec)){
 		foreach ($belong_has_one_spec as $belong_has_one) {
-			$attributes=$belong_has_one->attributes();  
-			AutoCodeDomain::$relation_all[$classname]['belong_has_one'][$attributes->name.""]=$belong_has_one."";  
-		}                              
-	}                          
+			$attributes=$belong_has_one->attributes();
+			AutoCodeDomain::$relation_all[$classname]['belong_has_one'][$attributes->name.""]=$belong_has_one."";
+		}
+	}
 	//导出一对多关系规范定义(如果存在)
 	$has_many_spec=$dataobject->has_many->relationclass;
-	if (!empty($has_many_spec)){         
+	if (!empty($has_many_spec)){
 		foreach ($has_many_spec as $has_many) {
-			$attributes=$has_many->attributes();  
-			AutoCodeDomain::$relation_all[$classname]['has_many'][$attributes->name.""]=$has_many."";  
-		}                             
-	}                                
+			$attributes=$has_many->attributes();
+			AutoCodeDomain::$relation_all[$classname]['has_many'][$attributes->name.""]=$has_many."";
+		}
+	}
 	//导出多对多关系规范定义(如果存在)
 	$many_many_spec=$dataobject->many_many->relationclass;
-	if (!empty($many_many_spec)){            
+	if (!empty($many_many_spec)){
 		foreach ($many_many_spec as $many_many) {
-			$attributes=$many_many->attributes();  
-			AutoCodeDomain::$relation_all[$classname]['many_many'][$attributes->name.""]=$many_many."";  
-		}                          
-	}                                       
+			$attributes=$many_many->attributes();
+			AutoCodeDomain::$relation_all[$classname]['many_many'][$attributes->name.""]=$many_many."";
+		}
+	}
 	//导出从属于多对多关系规范定义(如果存在)
 	$belongs_many_many_spec=$dataobject->belongs_many_many->relationclass;
-	if (!empty($belongs_many_many_spec)){        
+	if (!empty($belongs_many_many_spec)){
 		foreach ($belongs_many_many_spec as $belongs_many_many) {
-			$attributes=$belongs_many_many->attributes();  
-			AutoCodeDomain::$relation_all[$classname]['belongs_many_many'][$attributes->name.""]=$belongs_many_many."";  
-		}                                    
-	}  
+			$attributes=$belongs_many_many->attributes();
+			AutoCodeDomain::$relation_all[$classname]['belongs_many_many'][$attributes->name.""]=$belongs_many_many."";
+		}
+	}
 }
 ?>
