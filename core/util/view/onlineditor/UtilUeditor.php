c73154f00b8f9a2b9cda5f87ddd0c8de1b89f9f4
skygreen2001 <skygreen2001@gmail.com>
2014-08-19 08:03:12 +0800

    完善后台在线编辑器UEdiotr:阻止div转换成p

--- core/util/view/onlineditor/UtilUeditor.php
+++ core/util/view/onlineditor/UtilUeditor.php
@@ -33,6 +33,7 @@ class UtilUeditor extends Util
 
 	/**
 	 * 预加载UEditor的JS函数
+	 * 如何阻止div标签自动转换为p标签:http://fex-team.github.io/ueditor/#qa-allowDivToP
 	 * @param string $textarea_id 在线编辑器所在的内容编辑区域TextArea的ID
 	 * @param ViewObject $viewobject 表示层显示对象,只在Web框架中使用
 	 * @param string form_id  在线编辑器所在的Form的ID
@@ -46,7 +47,9 @@ class UtilUeditor extends Util
 				var ue_{$textarea_id};
 				function pageInit_ue_{$textarea_id}()
 				{
-					ue_{$textarea_id}=UE.getEditor('{$textarea_id}');
+					ue_{$textarea_id}=UE.getEditor('{$textarea_id}',{
+						allowDivTransToP: false
+					});
 				}
 				"
 			);
@@ -59,7 +62,8 @@ class UtilUeditor extends Util
 				function pageInit_ue_{$textarea_id}()
 				{
 					ue_{$textarea_id}=UE.getEditor('{$textarea_id}',{
-						toolbars:$configString
+						toolbars:$configString,
+						allowDivTransToP: false
 					});
 				}
 				"
