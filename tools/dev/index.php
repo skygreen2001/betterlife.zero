0a6cab26de988d27e7d8bc4b90be06f0a8403929
skygreen2001 <skygreen2001@gmail.com>
2015-06-10 10:14:56 +0800

    完善工程重用基于mac的兼容性

--- tools/dev/index.php
+++ tools/dev/index.php
@@ -1466,6 +1466,8 @@ function smartCopy($source, $dest, $options=array('folderPermission'=>0755,'file
 		} else {
 			$__dest=$dest;
 		}
+                $dest_dir=dirname($__dest);
+                UtilFileSystem::createDir($dest_dir);
 		$result=copy($source, $__dest);
 		chmod($__dest,$options['filePermission']);
 
